47acaabe0ad9f83234fbb0bc46511933d92cc68a
irlan.freitas <irlan.freitas@iteris.local>
2021-06-09 11:48:42 -0300

    feat: Deploy com Docker na nuvem
    * Possibidade de usar Docker para criação de imagens e containers para
      aplicações que utilizam Java com Spring Boot.
    * Necessário criar uma arquivo Dockerfile no diretório raiz da
      aplicação, para ensinar ao Docker como deve ser gerada a imagem dela.
    * É possível passar variáveis de ambiente utilizadas pela aplicação para
      o container Docker.
    * Realizado o deploy do Spring Boot em ambientes Cloud, como o Heroku.
    * Cada provedor Cloud pode exigir diferentes configurações adicionais a
      serem realizadas na aplicação, para que seja executada sem problemas

--- 12 - Spring Boot - Part III/forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ 12 - Spring Boot - Part III/forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -20,16 +20,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 // ! o SpringBootServletInitializer junto com o pom
 // ! modificado para o packaging ser o war e o tomcat
 // ! desabilitadas no empacotamento
-public class ForumApplication extends SpringBootServletInitializer {
+//public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ForumApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ForumApplication.class, args);
+    }
 
-	// ! Método que é necessário sobrescrever para que o empacotamento
-	// ! como war, funcione.
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
-	}
+    // ! Método que é necessário sobrescrever para que o empacotamento
+    // ! como war, funcione.
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(ForumApplication.class);
+//	}
+
 }
--- 12 - Spring Boot - Part III/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ 12 - Spring Boot - Part III/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -79,7 +79,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                 .antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR") // * Configurando o perfil que pode deletar
